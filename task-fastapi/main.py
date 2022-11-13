18f454b51938c682cf7e5ef6362c9d5e1f9b6090
Holt1337 <85517118+Holt1337@users.noreply.github.com>
2022-11-13 16:55:37 +0300

    update file main.py

--- task-fastapi/main.py
+++ task-fastapi/main.py
@@ -1,6 +1,6 @@
 from pip._vendor.requests import packages
 from  pydantic import BaseModel
-from typing import Union
+from typing import Union, List
 from fastapi import HTTPException
 from fastapi.responses import RedirectResponse
 from enum import Enum
@@ -23,8 +23,8 @@ class Timestampp(BaseModel):
 
 @app.post('/post')
 async def get_post(item: Timestampp):
-    item.id = 0
-    item.timestamp = 0
+    #item.id = 0
+    #item.timestamp = 0
     return item
 
 
@@ -38,30 +38,53 @@ class Dogs(BaseModel):
     pk: Union[int, None] = None
     kind: DogType
 
+dogs_list = []
+
+
 @app.get('/dog')
-async def get_dogs(dog: Dogs):
-    #dog.pk = 0
-    return dog
+async def get_dogs(kind: DogType = None):
+    list = []
+    for dog in dogs_list:
+        if dog.kind == kind:
+            list.append(dog)
 
-@app.post('/dog')
+    #dog.kind = kind
+    return list
+
+@app.post('/dog', response_model=Dogs)
 async def create_dog(dog: Dogs):
-    dog.pk = 0
+    if dog.pk:
+        1 == 1
+    else:
+        dog.pk = len(dogs_list)
+    dogs_list.append(dog)
     return dog
 
-@app.get('/dog/{pk}')
-async def get_dog_by_pk(pk: int, dog: Dogs):
-    dog.pk = pk
-    return dog
+@app.get('/dog/{pk}', response_model=Dogs)
+async def get_dog_by_pk(pk: int):
+    for dog in dogs_list:
+        if dog.pk == pk:
+            return dog
 
-@app.get('/dog/{type}')
-async def get_dog_by_type(type: DogType, dog: Dogs):
-    dog.kind = type
-    return dog
+    raise HTTPException(404)
 
-@app.patch('/dog/{pk}')
-async def update_dog(pk: int, dog: Dogs):
-    dog.pk = pk
-    return dog
+@app.get('/dog/{type}', response_model=Dogs)
+async def get_dog_by_type(type: DogType):
+    for dog in dogs_list:
+        if dog.kind == type:
+            return dog
 
+    raise HTTPException(404)
 
+@app.patch('/dog/{pk}', response_model=Dogs)
+async def update_dog(pk: int, ndog: Dogs):
+    for dog in dogs_list:
+        if dog.pk == pk:
+            dog.kind = ndog.kind
+            dog.name = ndog.name
+            dog.pk = ndog.pk
+
+            return dog
+
+    raise HTTPException(404)
 
